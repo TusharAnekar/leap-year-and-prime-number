ce1914fa798722881b82c47f31bdedea2dfa64f3
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-06-29 17:29:01 +0530

    js file added

---
+++ app.js
@@ -0,0 +1,26 @@
+var checkLeapYear = document.querySelector("#btn-checkLY");
+var inputLeapYear = document.querySelector('#inputNo');
+var outputDiv = document.querySelector("#output");
+
+checkLeapYear.addEventListener("click", clickHandler);
+
+function LeapYear(yearOfBirth) {
+
+    var result ="";
+
+    if((yearOfBirth % 4 ===0 && yearOfBirth % 100 !=0)||(yearOfBirth % 400 === 0)) {
+        result = "Your birth year is a leap year."
+        return (result);
+      }
+      
+      else{
+        result ="Your birth year is not a leap year.";
+        return(result);
+      }
+}
+
+function clickHandler() {
+    var inputLY = inputLeapYear.value;
+    fetch(LeapYear(inputLY))
+    .then( outputDiv.innerText = Response)
+}
